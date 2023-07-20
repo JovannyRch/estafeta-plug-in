69ddd6988ce9e793574c5da144bd34b385dfe40a
JovannyRch <jovannyrch@gmail.com>
2023-07-20 17:39:39 -0600

    Add paginator mock

--- web/frontend/components/Pagination/index.tsx
+++ web/frontend/components/Pagination/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { Container } from "./styled-components";
+import { Container, IconButton, NumberButton } from "./styled-components";
 
 const Pagination = () => {
-  return <Container>Pagination</Container>;
+  return (
+    <Container>
+      <IconButton>{"<<"}</IconButton>
+      <IconButton>{"<"}</IconButton>
+      <NumberButton active>1</NumberButton>
+      <NumberButton>2</NumberButton>
+      <NumberButton>3</NumberButton>
+      <IconButton active>{">"}</IconButton>
+      <IconButton active>{">>"}</IconButton>
+    </Container>
+  );
 };
 
 export default Pagination;
--- web/frontend/components/Pagination/styled-components.tsx
+++ web/frontend/components/Pagination/styled-components.tsx
@@ -2,4 +2,35 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  column-gap: 10px;
+`;
+
+export const IconButton = styled.div`
+  cursor: pointer;
+  font-family: Montserrat;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+
+  ${({ active = "false" }) =>
+    active.toString() === "true" ? "color: #12263c;" : "color: #C7C7C7;"}
+`;
+
+export const NumberButton = styled.div`
+  cursor: pointer;
+  font-family: Montserrat;
+  min-width: 20px;
+  height: 20px;
+  padding: 1px 7px;
+  border-radius: 3px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ active = "false" }) =>
+    active.toString() === "true"
+      ? "background-color: #46BB98;color: #fff;"
+      : "background-color: transparent; color: #12263C;"}
 `;
