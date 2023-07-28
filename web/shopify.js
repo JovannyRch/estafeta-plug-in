b164876afb34e112838aaedf548d67da79f813ba
JovannyRch <jovannyrch@gmail.com>
2023-07-28 16:46:51 -0600

    Add middleware

--- web/shopify.js
+++ web/shopify.js
@@ -1,7 +1,12 @@
-import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
+import {
+  BillingInterval,
+  LATEST_API_VERSION,
+  Session,
+} from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+import axios from "axios";
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
@@ -32,5 +37,65 @@ const shopify = shopifyApp({
   // This should be replaced with your preferred storage strategy
   sessionStorage: new SQLiteSessionStorage(DB_PATH),
 });
+
+export const tokenMiddleware = (req, res, next) => {
+  console.log("tokenMiddleware");
+
+  /* let appId = process.env.appId;
+  let appSecret = process.env.appSecret; */
+  let shop = req.query.shop;
+  let code = req.query.code;
+
+  const regex = /^[a-z\d_.-]+[.]myshopify[.]com$/;
+
+  //shopify.auth.callback(),
+
+  if (regex) {
+    let accessTokenRequestUrl = "https://" + shop + "/admin/oauth/access_token";
+    let accessTokenPayload = {
+      client_id: "228f9b3648e05552580fc62e32cd8b1f",
+      client_secret: "a54b2f31c9720a6571d112603f0ac9cb",
+      code,
+    };
+    axios
+      .post(accessTokenRequestUrl, accessTokenPayload)
+      .then((response) => {
+        let accessToken = response.data.access_token;
+
+        const shopData = {
+          shop,
+          accessToken,
+        };
+        console.log("shopData", shopData);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        console.log("finally");
+        next();
+      });
+  }
+};
+
+export const routingMiddleware = async (req, res) => {
+  console.log("routingMiddleware");
+  /* 
+  const session = shopify.config.sessionStorage.loadSession(req);
+  console.log("session", session); */
+
+  shopify.config.sessionStorage.storeSession({});
+  const host = shopify.api.utils.sanitizeHost(req.query.host);
+  const redirectUrl = shopify.api.config.isEmbeddedApp
+    ? await shopify.api.auth.getEmbeddedAppUrl({
+        rawRequest: req,
+        rawResponse: res,
+      })
+    : `/?shop=${res.locals.shopify.session.shop}&host=${encodeURIComponent(
+        host
+      )}`;
+  console.log(`Redirecting to host at ${redirectUrl}`);
+  res.redirect(`${redirectUrl}/orders`);
+};
 
 export default shopify;
